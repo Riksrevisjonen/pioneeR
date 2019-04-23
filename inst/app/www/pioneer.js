0b3754e1895a30aab79f68335b538fedc11eec03
Ove Haugland Jakobsen <ohj@Oves-RR-MacBook-Pro.local>
2019-04-23 23:20:56 +0200

    JS update

--- inst/app/www/pioneer.js
+++ inst/app/www/pioneer.js
@@ -2,6 +2,14 @@
 
 $(document).ready(function() {
 
+  // We want to disable the default behaviour of dropdown menus in Bootstrap so
+  // that the dropdown does not immediately close when the user selects an input
+  // value
+  $(document).on('click', '#uopts_menu', function(e) {
+     e.stopPropagation();
+  });
+
+  // Change the analysis tab based on if we have time series data or not
   $(document).on('shiny:inputchanged', function(event) {
     if (event.name === 'hasyear') {
       if (event.value === true) {
