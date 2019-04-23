9fec5fa126e1ec26f89c28eae37c5943665f5d12
Ove Haugland Jakobsen <ohj@Oves-RR-MacBook-Pro.local>
2019-04-23 23:22:02 +0200

    New arguments to runPioneeR function. Version bump.

--- R/shiny-functions.R
+++ R/shiny-functions.R
@@ -1,12 +1,33 @@
+#' @import shiny
+NULL
+
 #' Run pioneeR
 #'
 #' Run the pioneeR app on your local machine.
 #'
+#' @param x A data frame that should be loaded with the app. See details.
+#' @param port The TCP port that the application should listen on.
+#' @param ... Other arguments to send to \code{\link[shiny]{runApp}}
+#'
+#' @details
+#' You can load a data object in your current environment to the app. You can pass
+#' a data frame or a character string with the object name of the data frame you
+#' want to be loaded when the app launches. Note that you should only use data
+#' frame objects. If you have a tibble (from the tidyverse) or a data table, you
+#' can convert to an ordinary data.frame using \code{\link[base]{as.data.frame}}.
+#'
 #' @importFrom shiny runApp
 #'
 #' @export
-runPioneeR <- function() {
+runPioneeR <- function(x = NULL, port = NULL, ...) {
 
-  runApp(system.file('app', package = 'pioneeR'))
+  if (inherits(x, 'data.frame'))
+    Sys.setenv('PIONEER_DATA' = deparse(substitute(x)))
+  else if (inherits(x, 'character'))
+    Sys.setenv('PIONEER_DATA' = x)
+  else
+    Sys.setenv('PIONEER_DATA' = '')
+
+  runApp(system.file('app', package = 'pioneeR'), port = port)
 
 }
