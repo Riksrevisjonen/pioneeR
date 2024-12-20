81ef43a9d95bbbcf98179e5d9f14cedb5c47ff7f
Ove Haugland Jakobsen <ohj@mac.com>
2024-12-20 11:28:27 +0100

    Convert haven_labelled to factors. Closes #116

--- R/mod_file-upload.R
+++ R/mod_file-upload.R
@@ -1,16 +1,16 @@
 #' UI module for file upload function
-#' @noRd 
+#' @noRd
 file_upload_ui <- function(id, wrap = FALSE, ...) {
   ns <- NS(id)
   if (wrap) {
-    div(actionButton(ns('file_modal'), 'Upload file'), ...)
+    div(actionButton(ns("file_modal"), "Upload file"), ...)
   } else {
-    actionButton(ns('file_modal'), 'Upload file')
+    actionButton(ns("file_modal"), "Upload file")
   }
 }
 
 #' Server module for file upload function
-#' @noRd 
+#' @noRd
 file_upload_srv <- function(id) {
   moduleServer(
     id, function(input, output, session) {
@@ -30,20 +30,20 @@ file_upload_srv <- function(id) {
       observeEvent(input$file_modal, {
         showModal(
           modalDialog(
-            easyClose = TRUE, size = 'xl',
-            title = 'Upload file',
+            easyClose = TRUE, size = "xl",
+            title = "Upload file",
             footer = tagList(
-              actionButton(ns('file_save'), 'Save and close', class = 'btn-sm btn-primary', `data-bs-dismiss` = 'modal'),
-              bs_modal_button('Cancel', size = 'sm', color = 'secondary')
+              actionButton(ns("file_save"), "Save and close", class = "btn-sm btn-primary", `data-bs-dismiss` = "modal"),
+              bs_modal_button("Cancel", size = "sm", color = "secondary")
             ),
             div(
-              class = 'input-group mb-3',
+              class = "input-group mb-3",
               tags$input(
-                class = 'form-control', type = 'file', id = ns('fileInput'),
-                accept='text/csv,text/plain,text/comma-separated-values,.tsv,.csv,.xls,.xlsx,.dta,.rds,.sav')
+                class = "form-control", type = "file", id = ns("fileInput"),
+                accept="text/csv,text/plain,text/comma-separated-values,.tsv,.csv,.xls,.xlsx,.dta,.rds,.sav")
             ),
-            uiOutput(ns('file_opts')),
-            uiOutput(ns('clean_file'))
+            uiOutput(ns("file_opts")),
+            uiOutput(ns("clean_file"))
           )
         )
       }, ignoreInit = TRUE)
@@ -56,14 +56,14 @@ file_upload_srv <- function(id) {
       raw_file <- reactive({
         req(input$fileInput)
         if (is.null(file_params$ext)) return()
-        if (file_params$ext %in% c('csv', 'tsv', 'dsv', 'txt')) {
+        if (file_params$ext %in% c("csv", "tsv", "dsv", "txt")) {
           raw_doc <- readLines(input$fileInput$datapath, n = 1000)
-        } else if (file_params$ext %in% c('xlsx', 'xlsm', 'xls')) {
+        } else if (file_params$ext %in% c("xlsx", "xlsm", "xls")) {
           file_params$sheets <- suppressMessages(readxl::excel_sheets(input$fileInput$datapath))
           raw_doc <- suppressMessages(readxl::read_excel(input$fileInput$datapath))
-        } else if (file_params$ext == 'dta') {
+        } else if (file_params$ext == "dta") {
           raw_doc <- haven::read_dta(input$fileInput$datapath)
-        } else if (file_params$ext == 'sav') {
+        } else if (file_params$ext == "sav") {
           raw_doc <- haven::read_sav(input$fileInput$datapath)
         } else {
           return(NULL)
@@ -75,7 +75,7 @@ file_upload_srv <- function(id) {
         req(raw_file())
         ext <- file_params$ext
         if (is.null(ext)) return()
-        if (ext %in% c('csv', 'tsv', 'dsv', 'txt')) {
+        if (ext %in% c("csv", "tsv", "dsv", "txt")) {
           req(input$file_sep, input$file_dec)
           header <- if (is.null(input$file_header)) TRUE else as.logical(input$file_header)
           clean_doc <- utils::read.table(
@@ -85,10 +85,19 @@ file_upload_srv <- function(id) {
             quote = "\"",
             header = header,
             fill = TRUE)
-        } else if (ext %in% c('xlsx', 'xlsm', 'xls')) {
+        } else if (ext %in% c("dta")) {
+          clean_doc <- raw_file()
+          lbl_cols <- which(sapply(clean_doc, inherits, what = "haven_labelled"))
+          if (length(lbl_cols) > 0) {
+            lbl_cols <- unname(lbl_cols)
+            for (col in lbl_cols) {
+              clean_doc[[col]] <- haven::as_factor(clean_doc[[col]])
+            }
+          }
+        } else if (ext %in% c("xlsx", "xlsm", "xls")) {
           range <- NULL
           if (has_value(input$file_start_cell) && has_value(input$file_end_cell)) {
-            range <- sprintf('%s:%s', input$file_start_cell, input$file_end_cell)
+            range <- sprintf("%s:%s", input$file_start_cell, input$file_end_cell)
           }
           clean_doc <- suppressMessages(readxl::read_excel(
             input$fileInput$datapath,
@@ -115,55 +124,55 @@ file_upload_srv <- function(id) {
 
       output$file_opts <- renderUI({
         req(raw_file())
-        if (file_params$ext %in% c('csv', 'tsv', 'dsv', 'txt')) {
+        if (file_params$ext %in% c("csv", "tsv", "dsv", "txt")) {
           ui <- fluidRow(
             column(
               radioButtons(
-                ns('file_sep'), 'Separator',
-                c('Semicolon' = ';', 'Comma' = ',', 'Tabulator' = '\t'),
+                ns("file_sep"), "Separator",
+                c("Semicolon" = ";", "Comma" = ",", "Tabulator" = "\t"),
                 inline = TRUE),
               width = 3
             ),
             column(
               tagList(
-                p(class = 'mb-2', tags$strong('Headings')),
-                checkboxInput(ns('file_header'), NULL, value = TRUE)
+                p(class = "mb-2", tags$strong("Headings")),
+                checkboxInput(ns("file_header"), NULL, value = TRUE)
               ),
               width = 3
             ),
             column(
               radioButtons(
-                ns('file_dec'), 'Decimal',
-                c('Comma' = ',', 'Period' = '.'),
+                ns("file_dec"), "Decimal",
+                c("Comma" = ",", "Period" = "."),
                 inline = TRUE),
               width = 3
             ),
             column(
               selectizeInput(
-                ns('file_enc'), 'Encoding',
+                ns("file_enc"), "Encoding",
                 c(
-                  'UTF-8' = 'utf-8',
-                  'Windows (Western)' = 'cp1252',
-                  'ISO-8859-1' = 'latin-1')),
+                  "UTF-8" = "utf-8",
+                  "Windows (Western)" = "cp1252",
+                  "ISO-8859-1" = "latin-1")),
               width = 3
             )
           )
-        } else if (file_params$ext %in% c('xlsx', 'xlsm', 'xls')) {
+        } else if (file_params$ext %in% c("xlsx", "xlsm", "xls")) {
           ui <- fluidRow(
             column(
-              selectizeInput(ns('file_sheet'), 'Sheet', file_params$sheets),
+              selectizeInput(ns("file_sheet"), "Sheet", file_params$sheets),
               width = 3
             ),
             column(
-              numericInput(ns('file_skip'), 'Skip rows', 0, min = 0, step = 1),
+              numericInput(ns("file_skip"), "Skip rows", 0, min = 0, step = 1),
               width = 3
             ),
             column(
-              textInput(ns('file_start_cell'), 'Cell (start)', 'A1'),
+              textInput(ns("file_start_cell"), "Cell (start)", "A1"),
               width = 3
             ),
             column(
-              textInput(ns('file_end_cell'), 'Cell (end)', NULL, placeholder = 'Add cell reference'),
+              textInput(ns("file_end_cell"), "Cell (end)", NULL, placeholder = "Add cell reference"),
               width = 3
             )
           )
@@ -175,7 +184,7 @@ file_upload_srv <- function(id) {
           fluidRow(
             column(
               alert(
-                'If the data looks OK, click the Save button to add the data for analysis.',
+                "If the data looks OK, click the Save button to add the data for analysis.",
                 dismissable = TRUE
               ),
               width = 12
@@ -191,12 +200,12 @@ file_upload_srv <- function(id) {
           if (!is.null(d$error)) {
 
           } else {
-            return(alert('Whoopos. Something bad happened'))
+            return(alert("Whoopos. Something bad happened"))
           }
         }
         reactable(
           d, compact = TRUE, bordered = TRUE, sortable = FALSE,
-          striped = TRUE, rownames = FALSE, class = 'small', height = 'auto'
+          striped = TRUE, rownames = FALSE, class = "small", height = "auto"
         )
       })
 
